39f65637d2629a26a8732c5f0257b26029b3879c
Terrence Wong <terrencewong223@gmail.com>
2020-09-14 14:18:18 -0700

    added functions for 2

--- src/recursion.js
+++ src/recursion.js
@@ -21,6 +21,7 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  return array+sum(array);
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
